7d76bd1bbe70c99e800c09d84f4a987fa1aa6838
qaszz <32517805+qaszz@users.noreply.github.com>
2020-09-27 22:30:48 +0200

    Complete parsing of QueryString (#1)
    * new test is failing
    
    * second test is working
    new test if failing
    
    * previous test is working
    new test is failing
    
    * complete parsing of QueryString

--- src/main/java/no/kristiania/http_server/QueryString.java
+++ src/main/java/no/kristiania/http_server/QueryString.java
@@ -1,11 +1,20 @@
 package no.kristiania.http_server;
 
-public class QueryString {
-    public QueryString(String queryString){
+import java.util.HashMap;
+import java.util.Map;
 
+public class QueryString {
+    private Map<String, String> parameters = new HashMap<>();
+    public QueryString(String queryString){
+        for (String parameter : queryString.split("&")) {
+            int equalsPos = parameter.indexOf('=');
+            String value = parameter.substring(equalsPos+1);
+            String parameterName = parameter.substring(0, equalsPos);
+            parameters.put(parameterName, value);
+        }
     }
 
     public String getParameter(String name) {
-        return "200";
+        return parameters.get(name);
     }
 }
--- src/test/java/no/kristiania/http_server/QueryStringTest.java
+++ src/test/java/no/kristiania/http_server/QueryStringTest.java
@@ -6,8 +6,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class QueryStringTest {
     @Test
-    void shouldRetrieveQueryParemeter(){
-        QueryString queryString = new QueryString("status-200");
+    void shouldRetrieveQueryParameter(){
+        QueryString queryString = new QueryString("status=200");
         assertEquals("200", queryString.getParameter("status"));
     }
+    @Test
+    void shouldRetrieveOtherQueryParameter(){
+        QueryString queryString = new QueryString("status=404");
+        assertEquals("404", queryString.getParameter("status"));
+    }
+    @Test
+    void shouldRetrieveQueryParameterByName(){
+        QueryString queryString = new QueryString("text=Hello");
+        assertEquals(null, queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
+    }
+    @Test
+    void shouldHandleMultipleParameters(){
+        QueryString queryString = new QueryString("text=Hello&status=200");
+        assertEquals("200", queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
+    }
+
 }
