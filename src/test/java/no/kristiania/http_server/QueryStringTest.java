3d2a6339954ac2d75cb58628e7ec08d0d81a4ecf
qaszz <32517805+qaszz@users.noreply.github.com>
2020-09-27 22:21:09 +0200

    previous test is working new test is failing

--- src/test/java/no/kristiania/http_server/QueryStringTest.java
+++ src/test/java/no/kristiania/http_server/QueryStringTest.java
@@ -6,19 +6,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class QueryStringTest {
     @Test
-    void shouldRetrieveQueryParemeter(){
+    void shouldRetrieveQueryParameter(){
         QueryString queryString = new QueryString("status=200");
         assertEquals("200", queryString.getParameter("status"));
     }
     @Test
-    void shouldRetrieveOtherQueryParemeter(){
+    void shouldRetrieveOtherQueryParameter(){
         QueryString queryString = new QueryString("status=404");
         assertEquals("404", queryString.getParameter("status"));
     }
     @Test
-    void shouldRetrieveQueryParemeterByName(){
+    void shouldRetrieveQueryParameterByName(){
         QueryString queryString = new QueryString("text=Hello");
         assertEquals(null, queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
+    }
+    @Test
+    void shouldHandleMultipleParameters(){
+        QueryString queryString = new QueryString("text=Hello&status=200");
+        assertEquals("200", queryString.getParameter("status"));
         assertEquals("Hello", queryString.getParameter("text"));
     }
 
